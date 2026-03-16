46115e06689573ef419101365ef0ad15c7915012
illamparuthi <116744508+illamparuthi@users.noreply.github.com>
2026-03-16 12:50:37 +0530

    Update cfar.v

--- src/cfar.v
+++ src/cfar.v
@@ -7,19 +7,13 @@ module cfar (
     output reg        detect
 );
 
-reg [7:0] noise_avg;
-
 always @(posedge clk or posedge rst) begin
     if (rst) begin
-        noise_avg <= 8'd10;
         detect <= 1'b0;
     end
     else begin
-        // simple running average
-        noise_avg <= (noise_avg + sample_in) >> 1;
-
-        // detection rule
-        if (sample_in > (noise_avg << 2))
+        // spike detection
+        if (sample_in > 8'd50)
             detect <= 1'b1;
         else
             detect <= 1'b0;
