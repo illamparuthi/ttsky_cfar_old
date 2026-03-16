d80a421cc9db0dc5f706203e7e97ab59c4187bdd
illamparuthi <116744508+illamparuthi@users.noreply.github.com>
2026-03-16 12:48:41 +0530

    Update buzzer.v

--- src/buzzer.v
+++ src/buzzer.v
@@ -1,3 +1,5 @@
+`default_nettype none
+
 module buzzer(
     input clk,
     input rst,
@@ -5,23 +7,23 @@ module buzzer(
     output reg buzzer_out
 );
 
-reg [11:0] counter;
+reg [11:0] hold_counter;
 
 always @(posedge clk or posedge rst) begin
     if (rst) begin
-        counter <= 0;
-        buzzer_out <= 0;
+        hold_counter <= 12'd0;
+        buzzer_out <= 1'b0;
     end
     else if (detect) begin
-        counter <= 12'd2000;
-        buzzer_out <= 1;
+        hold_counter <= 12'd2000;
+        buzzer_out <= 1'b1;
     end
-    else if (counter > 0) begin
-        counter <= counter - 1;
-        buzzer_out <= 1;
+    else if (hold_counter > 0) begin
+        hold_counter <= hold_counter - 1;
+        buzzer_out <= 1'b1;
     end
     else begin
-        buzzer_out <= 0;
+        buzzer_out <= 1'b0;
     end
 end
 
