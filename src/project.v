7fc23a8274ff6133c1813d1424523c005959d09f
illamparuthi <116744508+illamparuthi@users.noreply.github.com>
2026-03-17 12:45:58 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -9,33 +9,19 @@ input  wire rst_n,
 input  wire ena
 );
 
-
 wire detect;
-wire buzzer_out;
-
-// FORCE input usage (important for synthesis)
-wire [7:0] data_in = ui_in;
 
 cfar cfar_inst (
-    .clk(clk),
-    .rst_n(rst_n),
-    .sample_in(data_in),
-    .detect(detect)
-);
-
-buzzer buzzer_inst (
-    .clk(clk),
-    .rst_n(rst_n),
-    .trigger(detect),
-    .out(buzzer_out)
+.clk(clk),
+.rst_n(rst_n),
+.sample_in(ui_in),
+.detect(detect)
 );
 
 assign uo_out[0] = detect;
-assign uo_out[1] = buzzer_out;
-assign uo_out[7:2] = 6'b0;
+assign uo_out[7:1] = 7'b0;
 
 assign uio_out = 8'b0;
 assign uio_oe  = 8'b0;
-
 
 endmodule
