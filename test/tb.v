8201c520ca5794b8397fb0da8cde9330d24c91bd
illamparuthi <116744508+illamparuthi@users.noreply.github.com>
2026-03-17 16:42:49 +0530

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -1,38 +1,47 @@
-`timescale 1ns/1ps
+`default_nettype none
+`timescale 1ns / 1ps
 
-module tb;
+module tb ();
 
-reg clk;
-reg rst_n;
-reg [7:0] ui_in;
-wire [7:0] uo_out;
+  // waveform dump
+  initial begin
+    $dumpfile("tb.fst");
+    $dumpvars(0, tb);
+  end
 
-tt_um_ttsky_cfar dut (
-    .clk(clk),
-    .rst_n(rst_n),
+  // Inputs
+  reg clk;
+  reg rst_n;
+  reg ena;
+  reg [7:0] ui_in;
+  reg [7:0] uio_in;
+
+  // Outputs
+  wire [7:0] uo_out;
+  wire [7:0] uio_out;
+  wire [7:0] uio_oe;
+
+`ifdef GL_TEST
+  wire VPWR = 1'b1;
+  wire VGND = 1'b0;
+`endif
+
+  // DUT
+  tt_um_ttsky_cfar user_project (
+
+`ifdef GL_TEST
+    .VPWR(VPWR),
+    .VGND(VGND),
+`endif
+
     .ui_in(ui_in),
     .uo_out(uo_out),
-    .uio_in(0),
-    .uio_out(),
-    .uio_oe(),
-    .ena(1)
-);
-
-initial clk = 0;
-always #5 clk = ~clk;
-
-initial begin
-    $dumpfile("dump.vcd");
-    $dumpvars(0, tb);
-
-    rst_n = 0;
-    ui_in = 0;
-
-    #50;
-    rst_n = 1;
-
-    #500;
-    $finish;
-end
+    .uio_in(uio_in),
+    .uio_out(uio_out),
+    .uio_oe(uio_oe),
+    .ena(ena),
+    .clk(clk),
+    .rst_n(rst_n)
+  );
 
 endmodule
