aa0efbb6cd9235aec4db0094fd13c24e94e5acd9
illamparuthi <116744508+illamparuthi@users.noreply.github.com>
2026-03-17 12:47:32 +0530

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -1,40 +1,56 @@
-`default_nettype none
 `timescale 1ns/1ps
 
-module tb();
+module tb;
 
 reg clk;
 reg rst_n;
-reg ena;
-reg [7:0] ui_in;
-reg [7:0] uio_in;
+reg [7:0] sample_in;
+wire detect;
 
-wire [7:0] uo_out;
-wire [7:0] uio_out;
-wire [7:0] uio_oe;
-
-tt_um_ttsky_cfar dut(
-    .ui_in(ui_in),
-    .uo_out(uo_out),
-    .uio_in(uio_in),
-    .uio_out(uio_out),
-    .uio_oe(uio_oe),
-    .ena(ena),
-    .clk(clk),
-    .rst_n(rst_n)
+cfar dut (
+.clk(clk),
+.rst_n(rst_n),
+.sample_in(sample_in),
+.detect(detect)
 );
 
-// Clock generation
+// clock
+always #5 clk = ~clk;
+
 initial begin
-    clk = 0;
-    forever #5 clk = ~clk;
+$dumpfile("dump.vcd");
+$dumpvars(0, tb);
+
+
+clk = 0;
+rst_n = 0;
+sample_in = 0;
+
+#20;
+rst_n = 1;
+
+// noise
+repeat (10) begin
+    sample_in = 10;
+    #10;
 end
 
-// Waveform dump — required by TinyTapeout CI
-initial begin
-    $dumpfile("tb.fst");
-    $dumpvars(0, tb);
-    #1;
+// spike
+repeat (5) begin
+    sample_in = 200;
+    #10;
+end
+
+// back to noise
+repeat (10) begin
+    sample_in = 10;
+    #10;
+end
+
+#100;
+$finish;
+
+
 end
 
 endmodule
