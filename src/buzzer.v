1eb86c57d8433e7fda56cf2ea1084017a07b71ee
illamparuthi <116744508+illamparuthi@users.noreply.github.com>
2026-03-17 11:10:26 +0530

    Update buzzer.v

--- src/buzzer.v
+++ src/buzzer.v
@@ -1,5 +1,7 @@
 `default_nettype none
 
+`default_nettype wire
+
 module buzzer (
 input  wire clk,
 input  wire rst,
@@ -7,15 +9,23 @@ input  wire detect,
 output reg  buzzer_out
 );
 
-```
+
+reg [11:0] counter;
+
 always @(posedge clk) begin
-    if (rst)
+    if (rst) begin
+        counter <= 0;
         buzzer_out <= 0;
-    else
-        buzzer_out <= detect;
+    end else begin
+        if (detect)
+            counter <= 12'd2000;
+        else if (counter > 0)
+            counter <= counter - 1;
+
+        buzzer_out <= (counter > 0);
+    end
 end
-```
+
 
 endmodule
 
-`default_nettype wire
