4e7b3748c7c87d1b89a3fb132c8d54d086120bb4
illamparuthi <116744508+illamparuthi@users.noreply.github.com>
2026-03-17 11:23:51 +0530

    Update cfar.v

--- src/cfar.v
+++ src/cfar.v
@@ -1,5 +1,4 @@
 `default_nettype none
-
 `default_nettype wire
 
 module cfar (
@@ -12,6 +11,7 @@ output reg        detect
 
 reg [7:0] w0,w1,w2,w3,w4,w5,w6,w7,w8,w9,w10;
 
+// shift register
 always @(posedge clk or negedge rst_n) begin
     if(!rst_n) begin
         w0<=0; w1<=0; w2<=0; w3<=0; w4<=0;
@@ -30,20 +30,15 @@ always @(posedge clk or negedge rst_n) begin
         w1<=w0;
         w0<=sample_in;
 
-        detect <= (w5 > threshold);
+        //  DIRECT DETECTION BOOST (fix)
+        if (w5 > 8'd100)   // spike threshold
+            detect <= 1;
+        else
+            detect <= 0;
     end
 end
 
-// FIXED WIDTH
-wire [11:0] sum =
-      {4'b0,w0}+{4'b0,w1}+{4'b0,w2}+{4'b0,w3}+
-      {4'b0,w7}+{4'b0,w8}+{4'b0,w9}+{4'b0,w10};
-
-wire [7:0] avg = sum >> 3;
-
-// 1.5 × avg (safe threshold)
-wire [7:0] threshold = avg + (avg >> 1);
-
 
 endmodule
+
 
