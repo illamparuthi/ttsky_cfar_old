e4c6d9be285ded58f55095bffbe54fb1ee65ca74
illamparuthi <116744508+illamparuthi@users.noreply.github.com>
2026-03-17 12:45:34 +0530

    Update cfar.v

--- src/cfar.v
+++ src/cfar.v
@@ -9,24 +9,29 @@ input  wire [7:0] sample_in,
 output reg        detect
 );
 
-
+reg [7:0] prev;
 reg [2:0] hold;
 
 always @(posedge clk or negedge rst_n) begin
-    if (!rst_n) begin
-        hold <= 0;
-        detect <= 0;
-    end else begin
-        // VERY SIMPLE condition → survives GL
-        if (sample_in[7] == 1'b1)   // detects values >=128
-            hold <= 3'd6;
-        else if (hold > 0)
-            hold <= hold - 1;
+if (!rst_n) begin
+prev   <= 0;
+detect <= 0;
+hold   <= 0;
+end else begin
+prev <= sample_in;
 
-        detect <= (hold > 0);
-    end
+
+    // spike detection
+    if (sample_in > prev + 8'd50)
+        hold <= 3'd6;
+    else if (hold > 0)
+        hold <= hold - 1;
+
+    detect <= (hold > 0);
 end
 
+
+end
 
 endmodule
 
