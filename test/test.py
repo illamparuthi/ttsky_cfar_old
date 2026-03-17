691a58070b31b7d21b1e08e0a138c640f20c37de
illamparuthi <116744508+illamparuthi@users.noreply.github.com>
2026-03-17 20:16:13 +0530

    Update test.py

--- test/test.py
+++ test/test.py
@@ -13,25 +13,23 @@ async def test_project(dut):
     dut.ui_in.value = 0
     dut.uio_in.value = 0
 
-    # Reset
     dut.rst_n.value = 0
     for _ in range(5):
         await RisingEdge(dut.clk)
+
     dut.rst_n.value = 1
 
     samples = [10,11,9,10,12,11,10,200,11,10]
 
     detected = False
 
-    # Feed samples
     for s in samples:
         dut.ui_in.value = s
         await RisingEdge(dut.clk)
 
-    # IMPORTANT: observe output long enough
-    for _ in range(50):   # increase window
+    # wait pipeline
+    for _ in range(20):
         await RisingEdge(dut.clk)
-
         if dut.uo_out.value[0] == 1:
             detected = True
 
